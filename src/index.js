fff6f3a02e49fc62cfcd029fbfedc7f317a24788
Sebastien <sanner.sebastien@gmail.com>
2021-04-29 19:05:57 +0200

    :art: add index.html header footer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import "../assets/stylesheets/styles.scss";
 function active() {let av = "salut"; console.log("Cela fonctionne encore avec Sass")};
 active();   
 
-document.write("Je debuteeeee à fond c est dur mais j'ai bientôt fini l'isntall des outils !!!!!!!! !");      
+document.write("Bonjour ici index.js !");      
   
 let a = "J'apprends Webpackkkk  !";        
- 
+  
 
