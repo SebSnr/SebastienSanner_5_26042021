ee8737beed9c461e196f33cc4956a762e2e76864
Sebastien <sanner.sebastien@gmail.com>
2021-05-06 12:55:51 +0200

    :wrench: add template product page html

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -7,65 +7,36 @@ import {getProductList} from "../api"
 
 export const showProduct2 = {
     render : async function () {
-        // let productlist = getProductList()
         // productlist.then(function (data){console.log(data.[0].name)});
-        let productList = await getProductList()
         // console.log(productList[0])
+        let productList = await getProductList()
 
         if (productList.error) {
             console.log ('erreur de la product list dans HomeScreen')
         }
 
-        let productCardHTML =             `
-                <div class="row">
-                        <div class="col-12 col-lg-4 mb-4">
-                            <div class="card h-100 rounded-6 shadow">
-                                <img src="${productList[0].imageUrl}" alt="" class="card-img-top">
-                                <div class="card-body d-flex flex-wrap justify-content-between">
-                                    <h2 class="card-title h5">${productList[0].name}</h2>
-                                    <a href="" class="stretched-link"></a>
-                                    <span class="card-text">${productList[0].price}</span>
+        let resultat = () => {
+            let productCards = ""
+            for(let element in productList){
+                productCards += `
+                            <div class="col-12 col-lg-4 mb-4">
+                                <div class="card h-100 rounded-6 shadow">
+                                    <img src="${productList[element].imageUrl}" alt="photo du produit" class="card-img-top">
+                                    <div class="card-body d-flex flex-wrap justify-content-between">
+                                        <h2 class="card-title h5">${productList[element].name}</h2>
+                                        <a href="" class="stretched-link"></a>
+                                        <span class="card-text">${productList[element].price}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                </div>
-            `
-        
-
-        // console.log(productCardHTML)
-
-        let resultat = () => {
-            let result = ""
-            for(let element in productList){
-               result += `
-               <div class="row">
-                       <div class="col-12 col-lg-4 mb-4">
-                           <div class="card h-100 rounded-6 shadow">
-                               <img src="${productList[element].imageUrl}" alt="" class="card-img-top">
-                               <div class="card-body d-flex flex-wrap justify-content-between">
-                                   <h2 class="card-title h5">${productList[element].name}</h2>
-                                   <a href="" class="stretched-link"></a>
-                                   <span class="card-text">${productList[element].price}</span>
-                               </div>
-                           </div>
-                       </div>
-               </div>
-           `
+                `
             }
-            return result
-
+            let rowContent = `<div class="row"> ${productCards} </div>`
+            return rowContent
         }
-        
-        // console.log(resultat())
-        
-
         return resultat()
     }
 }
-
-
-
-
 
 
 // getProductList().then(data => {
