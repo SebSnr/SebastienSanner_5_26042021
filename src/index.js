ed851a7830807af716d5a65c453ceb3af8a0dca8
Sebastien <sanner.sebastien@gmail.com>
2021-05-06 18:02:58 +0200

    :fire: restructure files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,70 @@
-
 import "../assets/stylesheets/styles.scss"
 
-import {showProduct} from "./homePage"
+/******************* get products list *******************/
+const getProductList = async function () {
+    return fetch('http://localhost:3000/api/teddies')
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log("API issue : code ${response.status}")
+                    return
+                }
 
+                return response.json()
+                    .then(function(data){
+                        // console.log(data)
+                        return data
+                    })
+            }
+        )
+        .catch(
+            function(err) {
+                console.log("fetch error", err)
+            }
+        )
+}
+
+/******************* show product cards *******************/
+const showProduct = {
+    render : async function () {
+        // productlist.then(function (data){console.log(data.[0].name)});
+        // console.log(productList[0])
+        let productList = await getProductList()
+
+        if (productList.error) {
+            console.log ('erreur de la product list dans HomeScreen')
+        }
+
+        let resultat = () => {
+            let productCards = ""
+            for(let element in productList){
+                productCards += `
+                            <div class="col-12 col-lg-4 mb-4">
+                                <div class="card h-100 rounded-6 shadow">
+                                    <img src="${productList[element].imageUrl}" alt="photo du produit" class="card-img-top">
+                                    <div class="card-body d-flex flex-wrap justify-content-between">
+                                        <h2 class="card-title h5">${productList[element].name}</h2>
+                                        <a id="bouttona" href="/public/product.html#${productList[element]._id}" class="stretched-link"></a>
+                                        <span class="card-text">${productList[element].price}</span>
+                                    </div>
+                                </div>
+                            </div>
+                `
+            }
+            let rowContent = `<div class="row"> ${productCards} </div>`
+            return rowContent
+        }
+        return resultat()
+    }
+}
+
+
+/******************* insert all HTML code *******************/
 const router = async function() {
     const mainContent = document.getElementById('maincontent')
     mainContent.innerHTML = await showProduct.render()
 }
-
-window.addEventListener('load', router);
+router()
+// window.addEventListener('load', router());
 
 
