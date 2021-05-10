c77b01dc59ce58a43ad6793355c64c7e5235d1e3
Sebastien <sanner.sebastien@gmail.com>
2021-05-10 13:49:34 +0200

    :art: start add data to localStorage

--- front-end/src/product.js
+++ front-end/src/product.js
@@ -16,6 +16,11 @@ const getProductDetails = () => {
                         const mainContent = document.getElementById('main-content')
                         mainContent.innerHTML = showProductDetails(data)
                         console.log(data)
+                        let addCartBtn = document.getElementById('addCart')
+                        addCartBtn.addEventListener('click', function(e){
+                            addCart(data)
+
+                        })
                     })
             } 
         )
@@ -56,24 +61,26 @@ const showProductDetails = (product) => {
                 ${createOptionList(product.colors)}
                 </select>
                 <br><br><br>
-                <table>
-                    <tr>
-                        <td class="col-1">
-                            <label for="productName">quantité</label>
-                        </td>
-                        <td class="col-3">
-                            <input type="number" class="form-control col-2" id="productName" aria-describedby="saisie quantité" value="1">
-                        </td>
-                        <td class="col-1">
+                <form>
+                    <table>
+                        <tr>
+                            <td class="col-1">
+                                <label for="productName">quantité</label>
+                            </td>
+                            <td class="col-3">
+                                <input type="number" class="form-control col-2" id="productName" aria-describedby="saisie quantité" value="1">
+                            </td>
+                            <td class="col-1">
 
-                        </td>
-                        <td class="col-7">
-                            <button type="button" class="btn btn-success col-12" id="addToCart">
-                                Ajouter au panier 
-                            </button>
-                        </td>
-                    </tr>
-                </table>
+                            </td>
+                            <td class="col-7">
+                                <button type="button" class="btn btn-success col-12" id="addCart">
+                                    Ajouter au panier 
+                                </button>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
                 <br><br>
                 <p>${product.description}</p>
             </div>
@@ -83,4 +90,48 @@ const showProductDetails = (product) => {
     `
 }
 
+
 getProductDetails()
+
+
+// Quand je click sur bouton, je crée un tableau dans le local storage
+//si tableau déjà présent,je rajouter un ligne 
+
+const addCart = (product) => {
+    let quantity = 9 //  update
+
+    // si aucun élément déjà ajouté dans le local storage, alors créer un array OrinocoBasket dans le local storage
+    if (window.localStorage.getItem('OrinocoBasket') == null) {
+        let itemsList = [[product, quantity]]
+        window.localStorage.setItem('OrinocoBasket', JSON.stringify(itemsList))
+    }
+    // sinon ajouter le produit et la quantité selectionné dans le array déjà créé dans le local storage
+    else {
+        let existingLocalStorage = JSON.parse(window.localStorage.getItem('OrinocoBasket'))
+        existingLocalStorage.push([product, quantity])
+        window.localStorage.setItem ('OrinocoBasket', JSON.stringify(existingLocalStorage))  
+    }
+    
+    console.log (JSON.parse(window.localStorage.getItem('OrinocoBasket')))
+}
+
+
+// pour mes vérifications perso sur console.log
+// let voir = JSON.parse(window.localStorage.getItem('OrinocoBasket'))
+window.localStorage.removeItem('OrinocoBasket')
+// let a = voir[0][0]._id //voir l'id du produit
+// console.log (a)
+// console.log (voir)
+console.log (JSON.parse(window.localStorage.getItem('OrinocoBasket')))
+
+
+
+
+
+
+
+
+
+
+
+
