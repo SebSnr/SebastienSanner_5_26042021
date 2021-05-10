0af620f8dd2ab619753bcdda358ba5ebcc9d32d9
Sebastien <sanner.sebastien@gmail.com>
2021-05-11 00:42:18 +0200

    :art: start localstorage from order page

--- front-end/src/order.js
+++ front-end/src/order.js
@@ -1,4 +1,59 @@
-    let storageAdress = localStorage.getItem('OrinocoBasket')
-    let storageData = JSON.parse(storageAdress)
 
-    document.getElementById('storageTest').innerHTML = storageData[1][0].name
+let cart = JSON.parse(localStorage.getItem('cart'))
+
+function showCart () {
+    let productItem = ""
+    for(i in cart){
+        let item = cart[i]
+        productItem += `
+        
+            <tr class="align-middle">
+                <td>
+                    <div class="media text-left align-middle">
+                        <a class="thumbnail " href="#"><img class="media-object" src="${item.imageUrl}" height="80px"></a>
+                    </div>
+                </td>
+                <td lass="text-left align-middle">${item.name}</td>
+                <td class="text-center align-middle">
+                    <input type="number" class="form-control col-2" id="quantity" aria-describedby="saisie quantité" value="${item.quantity}">
+                </td>
+                <td class="text-center align-middle">$14.61</td>
+                <td class="text-center">
+                    <button type="button" class="btn btn-outline-danger deleteItemBtn">
+                        <!-- <img class="red" src="./assets/icons/corbeille.svg" width="17px"alt="bouton supprimer produit"> -->
+                        <i class="fa fa-trash" aria-hidden="true"></i>
+                    </button>
+                </td>
+            </tr>
+
+        `
+        // console.log(item.productId)
+    }
+
+    return productItem
+}
+
+function deleteItem (index) {
+
+    console.log("Bravo la valeur a été supprimé")
+            
+    cart.splice(index, 1)   //doit supprimer, splice prends normalement 3 arg
+
+    showCart()
+
+    console.log("rien ne se passe de plus ? ")
+}
+
+function deleteAllCart () {
+    localStorage.removeItem('cart')
+}
+// deleteAllCart()
+
+
+document.getElementById('cartTable').innerHTML = showCart(cart)
+
+// let deleteItemBtn = document.getElementsByClassName('deleteItemBtn')
+// deleteItemBtn.addEventListener('click', function(e){
+// })
+
+console.log(JSON.parse(localStorage.getItem('cart')))
--- front-end/src/product.js
+++ front-end/src/product.js
@@ -1,8 +1,8 @@
 
 /******************* get product ID from url *******************/
-    let productId = document.location.hash.replace('#', '')
+let productId = document.location.hash.replace('#', '')
 
-/******************* get product details *******************/
+/******************* get product details from API *******************/
 const getProductDetails = () => {
     return fetch(`http://localhost:3000/api/teddies/${productId}`)
         .then(
@@ -15,13 +15,8 @@ const getProductDetails = () => {
                     .then(function(data){
                         const mainContent = document.getElementById('main-content')
                         mainContent.innerHTML = showProductDetails(data)
-                        console.log(data)
-                        //si click sur bouton
                         let addCartBtn = document.getElementById('addCart')
-                        addCartBtn.addEventListener('click', function(e){
-                            addCart(data)
-
-                        })
+                        addCartBtn.addEventListener('click', () => addCart(data))
                     })
             } 
         )
@@ -31,6 +26,8 @@ const getProductDetails = () => {
             }
         )
 }
+
+/******************* render product page content *******************/
 
 const showProductDetails = (product) => {
     if (product.error) {
@@ -59,29 +56,27 @@ const showProductDetails = (product) => {
                 <span><big>${product.price} €</big></span>
                 <br><br>
                 <select>
-                ${createOptionList(product.colors)}
+                    ${createOptionList(product.colors)}
                 </select>
                 <br><br><br>
-                <form>
-                    <table>
-                        <tr>
-                            <td class="col-1">
-                                <label for="productName">quantité</label>
-                            </td>
-                            <td class="col-3">
-                                <input type="number" class="form-control col-2" id="quantity" aria-describedby="saisie quantité" value="1">
-                            </td>
-                            <td class="col-1">
+                <table>
+                    <tr>
+                        <td class="col-1">
+                            <label for="productName">quantité</label>
+                        </td>
+                        <td class="col-3">
+                            <input type="number" class="form-control col-2" id="quantity" aria-describedby="saisie quantité" value="1">
+                        </td>
+                        <td class="col-1">
 
-                            </td>
-                            <td class="col-7">
-                                <a href="./order.html" type="button" class="btn btn-success col-12" id="addCart">
-                                    Acheter
-                                </a>
-                            </td>
-                        </tr>
-                    </table>
-                </form>
+                        </td>
+                        <td class="col-7">
+                            <a href="./order.html" type="button" class="btn btn-success col-12" id="addCart">
+                                Acheter
+                            </a>
+                        </td>
+                    </tr>
+                </table>
                 <br><br>
                 <p>${product.description}</p>
             </div>
@@ -91,82 +86,22 @@ const showProductDetails = (product) => {
     `
 }
 
+/******************* add product to cart *******************/
+
+function addCart (product) {
+    
+    let quantity = document.getElementById('quantity').value
+
+    let cart = []
+
+    if (localStorage.getItem('cart')){
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+    cart.push({'quantity' : quantity, 'Id' : product._id, 'name' : product.name, 'price' : product.price, 'imageUrl' : product.imageUrl, 'description' : product.description})
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
 
 getProductDetails()
 
-// Quand je click sur bouton, je crée un tableau dans le local storage
-//si tableau déjà présent,je rajouter un ligne 
-
-
-const addCart = (product) => {
-    
-    let quantity = document.getElementById('quantity').value //  update
-
-    let storageAdress = localStorage.getItem('OrinocoBasket')
-    let storageData = JSON.parse(storageAdress)
-
-
-    // si aucun élément déjà ajouté dans le local storage, alors créer un array OrinocoBasket dans le local storage
-    if (storageAdress == null) {
-        let itemsList = [[product, quantity]]
-        localStorage.setItem('OrinocoBasket', JSON.stringify(itemsList))
-    }
-
-    else {
-        // si l'élément existe déjà dans le array du local storage, alors remplacer cet élément par le nouveau
-        // if (storageData[0][0].name) {
-
-
-        if (storageData[0][0].name.includes(product.name)) { // a changer
-            
-            console.log("Bravo la valeur existe déjà")
-            
-            let newStorageData = storageData.splice(storageData[0], 1, [product, quantity])
-            localStorage.setItem ('OrinocoBasket', JSON.stringify(newStorageData))  
-            
-            console.log (storageData)
-            
-            
-        }
-        
-        // sinon ajouter le produit et la quantité selectionné dans le array déjà créé dans le local storage
-        else {
-            let existingLocalStorage = storageData
-            existingLocalStorage.push([product, quantity])
-            localStorage.setItem ('OrinocoBasket', JSON.stringify(existingLocalStorage))  
-        }
-    }
-    
-    // console.log (storageData)
-}
-
-const DeleteFromCart = () => {
-
-    console.log("Bravo la valeur a été supprimé")
-            
-    let newStorageData = storageData.splice(storageData[0], 1, )   //doit supprimer, splice prends normalement 3 arg
-    localStorage.setItem ('OrinocoBasket', JSON.stringify(newStorageData))  
-    
-    console.log (storageData)
-
-}
-
-const deleteCart = () => {
-    localStorage.removeItem('OrinocoBasket')
-
-}
-
-// let a = voir[0][0]._id //voir l'id du produit
-
-console.log(JSON.parse(localStorage.getItem('OrinocoBasket')))
-
-
-
-
-
-
-
-
-
-
-
+console.log(JSON.parse(localStorage.getItem('cart')))
